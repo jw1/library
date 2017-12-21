9a6b73f5a35aa040f4e32fe429cf153ffcee2eb1
James Warlick <jwarlick@financialpayments.com>
2017-12-21 13:52:49 -0600

    add mysql support before switching to mongo

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 var express = require('express');
-
 var app = express();
 
 var port = process.env.PORT || 5000;
 
-
 app.use(express.static('public'));
-app.set('views', './views');
-app.set('view engine', 'pug');
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+var nav = [
+    {Link: '/Books', Text: 'Books'},
+    {Link: '/Authors', Text: 'Authors'}
+];
+
+
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+app.use('/Books', bookRouter);
 
 app.get('/', function (request, response) {
-    response.render('index', {list: ['a', 'b']});
+    response.render(
+        'index',
+        {
+            title: 'Hello from render',
+            nav: [
+                {Link: '/Books', Text: 'Books'},
+                {Link: '/Authors', Text: 'Authors'}
+            ]
+        });
 });
 
 app.get('/books', function (request, response) {
